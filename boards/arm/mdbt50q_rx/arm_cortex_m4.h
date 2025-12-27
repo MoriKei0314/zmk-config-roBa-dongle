c560d92f3427a0ae9a662bf97a3d81fcaaabe788
MoriKei0314 <kesuikerimo0112@gmail.com>
2025-12-27 23:05:33 +0900

    Create arm_cortex_m4.h

---
+++ boards/arm/mdbt50q_rx/arm_cortex_m4.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _ARM_CORTEX_M4_H_
+#define _ARM_CORTEX_M4_H_
+
+/* This file is intentionally left minimal.
+ * It exists to satisfy the ARM Cortex M4 requirements
+ * in Zephyr for nRF52840 boards.
+ */
+
+#endif /* _ARM_CORTEX_M4_H_ */
